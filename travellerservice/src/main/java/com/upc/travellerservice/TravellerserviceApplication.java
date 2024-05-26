67923478115621e3de1f462023d940448e3261bc
AnthonyNunez500 <u202119276@upc.edu.pe>
2024-05-25 23:49:36 -0500

    update: Traveller properties

---
+++ travellerservice/src/main/java/com/upc/travellerservice/TravellerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.upc.travellerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellerserviceApplication.class, args);
+	}
+
+}
---
+++ travellerservice/src/test/java/com/upc/travellerservice/TravellerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.travellerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
