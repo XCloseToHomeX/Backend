70998d53768ebf8832c90d8fc4af1da45190d687
Rut <u20201c163@upc.edu.pe>
2024-06-13 22:46:41 -0500

    edit format

--- messaging-service/src/main/java/messaging/service/chat/ChatMessage.java
+++ messaging-service/src/main/java/messaging/service/chat/ChatMessage.java
@@ -1,14 +1,45 @@
 package messaging.service.chat;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class ChatMessage {
+    @JsonProperty("content")
     private String content;
+
+    @JsonProperty("sender")
     private String sender;
     //private MessageType type;
+
+    public ChatMessage() {}
+
+    public ChatMessage(String content, String sender) {
+        this.content = content;
+        this.sender = sender;
+    }
+
+    // Getters and Setters
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    @Override
+    public String toString() {
+        return "ChatMessage{" +
+                "content='" + content + '\'' +
+                ", sender='" + sender + '\'' +
+                '}';
+    }
 }
